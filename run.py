71294af071b11838d52728aa4fe3bbb608472a91
Archuqq <sw1pe.stand@gmail.com>
2025-03-27 23:16:35 +0300

    Add Render configuration

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-from app import app, socketio
 import os
+from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, 
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app,
                 host='0.0.0.0',
-                port=int(os.environ.get('PORT', 5000)),
+                port=port,
                 debug=False,
                 allow_unsafe_werkzeug=True) 
